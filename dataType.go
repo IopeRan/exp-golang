99e782035835eb59ce90e39c3307be73bdd98ff5
Erlang Andriyanputra <chilliope48@gmail.com>
2024-02-05 15:37:54 +0800

    string manipulate

---
+++ dataType.go
@@ -0,0 +1,18 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	// Type Data
+
+// 	// Number
+// 	fmt.Println(1) // Integer
+// 	fmt.Println(1.1) // Float
+
+// 	// Text
+// 	fmt.Println("Erlang Andriyanputra") // String
+
+// 	// Boolean
+// 	fmt.Println(false)
+// 	fmt.Println(true)
+// }
---
+++ string.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Erlang Andriyanputra")
+	fmt.Println("Erlang" + "" + "Andriyanputra")
+	fmt.Println(len("Erlang"))
+	fmt.Println("Erlang"[0])
+	fmt.Println("Erlang"[2:6])
+	fmt.Println("Erlang"[0:])
+}
